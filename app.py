0f966b9b4dc333b3076357e22a420163cffe6353
Rendu129 <edrs129@gmail.com>
2024-01-10 23:38:47 -0600

    app file

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+car_data = pd.read_csv(
+    r"C:\Users\ed_rs\OneDrive\Documentos\Programacion\Tripleten\Proyecto_Spring5\Project_Spring5\vehicles_us.csv")
