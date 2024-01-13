3b154b5caae85d04b18a12a9932870f1a43aaeb8
Rendu129 <edrs129@gmail.com>
2024-01-13 17:51:57 -0600

    new

--- app.py
+++ app.py
@@ -3,3 +3,7 @@ import pandas as pd
 import plotly_express as px
 car_data = pd.read_csv(
     r"C:\Users\ed_rs\OneDrive\Documentos\Programacion\Tripleten\Proyecto_Spring5\Project_Spring5\vehicles_us.csv")
+
+st.header('Venta de Autos')
+st.write('Esta app esta en construccion')
+fig = px.histogram(car_data, x='odometer')
