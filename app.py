eea5bbafc69cbe306a8a39d064b15144984366aa
Rendu129 <edrs129@gmail.com>
2024-01-13 18:16:57 -0600

    modified01

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pandas as pd
 import plotly_express as px
-car_data = pd.read_csv(
-    r"C:\Users\ed_rs\OneDrive\Documentos\Programacion\Tripleten\Proyecto_Spring5\Project_Spring5\vehicles_us.csv")
+car_data = pd.read_csv('vehicles_us.csv')
 
 st.header('Venta de Autos')
-st.write('Esta app esta en construccion')
 
 hist_button = st.button('Construir histograma')
-if his_button:
+if hist_button:
     st.write(
         'Construir un histograma para el conjunto de datos de anuncios de venta de coches')
     fig = px.histogram(car_data, x='odometer')
