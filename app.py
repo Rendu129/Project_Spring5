ffca03a362ac0c638cdb94478ea9a9121801964a
Rendu129 <edrs129@gmail.com>
2024-01-14 00:07:08 -0600

    app 02

--- app.py
+++ app.py
@@ -23,6 +23,19 @@ if disp_button:
     fig = px.scatter(car_data, x='model_year', y='price')
     st.plotly_chart(fig, use_container_widh=True)
 
+automatic = car_data.query("transmission=='automatic'")[
+    ['price', 'model_year']]
+manual = car_data.query("transmission=='manual'")[['price', 'model_year']]
+other = car_data.query("transmission=='other'")[['price', 'model_year']]
+
 list = ['automatic', 'manual', 'other']
 option = st.selectbox(
-    'Seleciona la condicion que deseas tu auto', list)
+    'Seleciona tipo de transmision', list, index=None)
+st.write('Haz seleccionado:', option)
+
+if option == 'automatic':
+    st.write('Selecciona tipo de grafico para la transmision automatica:')
+elif option == 'manual':
+    st.write('Selecciona tipo de grafico para la trasmision manual:')
+elif option == 'other':
+    st.write('Selecciona tipo de grafico para trasmision electrica')
