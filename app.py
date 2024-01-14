5a72ddb71de5f02ee4b96ea3e7fc12ae1bf97231
Rendu129 <edrs129@gmail.com>
2024-01-13 18:08:52 -0600

    graficos

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ car_data = pd.read_csv(
 
 st.header('Venta de Autos')
 st.write('Esta app esta en construccion')
-fig = px.histogram(car_data, x='odometer')
-print(car_data.head())
+
+hist_button = st.button('Construir histograma')
+if his_button:
+    st.write(
+        'Construir un histograma para el conjunto de datos de anuncios de venta de coches')
+    fig = px.histogram(car_data, x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
